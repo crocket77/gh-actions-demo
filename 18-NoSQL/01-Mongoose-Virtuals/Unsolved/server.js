b81a723ec57edcd0bd527add647c4e3b2e2395e7
Walter Perry <walterjamesperry@gmail.com>
2022-04-18 20:58:31 -0500

    push solved Activity 1 and Activity 2
    push solved Activity 1 and Activity 2

--- 18-NoSQL/01-Mongoose-Virtuals/Unsolved/server.js
+++ 18-NoSQL/01-Mongoose-Virtuals/Unsolved/server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const { User } = require('./models');
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/userdb',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/virtualsdb',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,13 +20,21 @@ mongoose.connect(
 mongoose.set('debug', true);
 
 app.post('/submit', ({ body }, res) => {
-  User.create(body)
+  const user = new User(body);
+
+  User.create(user)
     .then(dbUser => {
       res.json(dbUser);
     })
     .catch(err => {
       res.json(err);
     });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then(users => {
+    res.json(users);
+  });
 });
 
 app.listen(PORT, () => {
